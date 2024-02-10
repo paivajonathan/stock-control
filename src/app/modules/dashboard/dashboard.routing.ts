a9ed928b4cfd7122136907bc991dba0d5fd19c93
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-02-10 01:09:31 -0300

    feat(project): add dashboard home component

---
+++ src/app/modules/dashboard/dashboard.routing.ts
@@ -0,0 +1,9 @@
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { Routes } from "@angular/router";
+
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: '',
+    component: DashboardHomeComponent
+  }
+];
