d60887be923057952905ded39f3e2e65fd6bd917
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-02-11 18:20:41 -0300

    feat(dashboard home): implement products service on dashboard home

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: []
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  productsList: Array<GetAllProductsResponse> = [];
 
+  constructor(
+    private productService: ProductsService,
+    private messageService: MessageService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getProductsData();
+  }
+
+  getProductsData(): void {
+    this.productService
+      .getAllProducts()
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsList = response;
+            console.log(this.productsList);
+          }
+        },
+        error: (error) => {
+          console.log(error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          })
+        }
+      })
+  }
 }
