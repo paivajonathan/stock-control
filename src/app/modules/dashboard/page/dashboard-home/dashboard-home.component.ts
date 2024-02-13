6b4c8d50d4b88e8e45c683dfe6184b95bf9de599
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-02-12 23:50:29 -0300

    feat(dashboard): add products amount bar chart

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -4,21 +4,25 @@ import { ProductsService } from 'src/app/services/products/products.service';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 import { Subject, takeUntil } from 'rxjs';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class DashboardHomeComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   productsList: Array<GetAllProductsResponse> = [];
 
+  productsChartData!: ChartData;
+  productsChartOptions!: ChartOptions;
+
   constructor(
     private productService: ProductsService,
     private messageService: MessageService,
-    private productsDataTransferService: ProductsDataTransferService,
-  ) { }
+    private productsDataTransferService: ProductsDataTransferService
+  ) {}
 
   ngOnInit(): void {
     this.getProductsData();
@@ -33,6 +37,7 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
           if (response.length > 0) {
             this.productsList = response;
             this.productsDataTransferService.setProductsData(this.productsList);
+            this.setProductsChartConfig();
           }
         },
         error: (error) => {
@@ -42,9 +47,66 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
             summary: 'Erro',
             detail: 'Erro ao buscar produtos',
             life: 2500,
-          })
-        }
-      })
+          });
+        },
+      });
+  }
+
+  setProductsChartConfig(): void {
+    if (this.productsList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary =
+        documentStyle.getPropertyValue('--text-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartData = {
+        labels: this.productsList.map((element) => element.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productsList.map((element) => element?.amount),
+          },
+        ],
+      };
+
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500,
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    }
   }
 
   ngOnDestroy(): void {
